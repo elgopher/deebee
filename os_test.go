b661531daa6d2689f3cc11431b4472fdad355036
Jacek Olszak <jacekolszak@gmail.com>
2021-01-24 17:26:39 +0100

    Implement new Dir abstraction in OsDir

--- os_test.go
+++ os_test.go
@@ -2,96 +2,63 @@ package deebee_test
 
 import (
 	"io/ioutil"
-	"path/filepath"
 	"testing"
 
 	"github.com/jacekolszak/deebee"
-	"github.com/stretchr/testify/assert"
+	"github.com/jacekolszak/deebee/test"
 	"github.com/stretchr/testify/require"
 )
 
-var data = []byte("test")
+var dirs = map[string]func(t *testing.T) deebee.Dir{
+	"root":   rootDir,
+	"nested": makeNestedDir,
+}
 
-// TODO Reuse tests from fake package - behaviour of all adapters should be defined in well written reusable tests
+func rootDir(t *testing.T) deebee.Dir {
+	return deebee.OsDir(createTempDir(t))
+}
+
+func makeNestedDir(t *testing.T) deebee.Dir {
+	dir := rootDir(t)
+	err := dir.Mkdir("nested")
+	require.NoError(t, err)
+	return dir.Dir("nested")
+}
+
 func TestOsDir_FileWriter(t *testing.T) {
-	t.Run("should open new file", func(t *testing.T) {
-		osDir := deebee.OsDir(createTempDir(t))
-		// when
-		writer, err := osDir.FileWriter("file")
-		require.NoError(t, err)
-		defer writer.Close()
-		require.NoError(t, err)
-		assert.NotNil(t, writer)
-	})
+	test.TestDir_FileWriter(t, dirs)
+}
 
-	t.Run("should return error when file already exists", func(t *testing.T) {
-		dir := createTempDir(t)
-		writeFile(t, dir, "existing-file")
-		osDir := deebee.OsDir(dir)
-		// when
-		writer, err := osDir.FileWriter("existing-file")
-		require.Error(t, err)
-		assert.Nil(t, writer)
-	})
-
-	t.Run("should open file for write", func(t *testing.T) {
-		osDir := deebee.OsDir(createTempDir(t))
-		// when
-		writer, err := osDir.FileWriter("file")
-		require.NoError(t, err)
-		// then
-		_, err = writer.Write(data)
-		require.NoError(t, err)
-		err = writer.Close()
-		require.NoError(t, err)
-	})
+func TestFileWriter_Write(t *testing.T) {
+	test.TestFileWriter_Write(t, dirs)
 }
 
 func TestOsDir_FileReader(t *testing.T) {
-	t.Run("should return error when file does not exist", func(t *testing.T) {
-		osDir := deebee.OsDir(createTempDir(t))
-		// when
-		reader, err := osDir.FileReader("not-existing-file")
-		require.Error(t, err)
-		assert.Nil(t, reader)
-	})
+	test.TestDir_FileReader(t, dirs)
+}
 
-	t.Run("should open existing file", func(t *testing.T) {
-		dir := createTempDir(t)
-		osDir := deebee.OsDir(dir)
-		writeFile(t, dir, "existing-file")
-		// when
-		reader, err := osDir.FileReader("existing-file")
-		require.NoError(t, err)
-		defer reader.Close()
-		// then
-		require.NoError(t, err)
-		assert.NotNil(t, reader)
-	})
+func TestFileReader_Read(t *testing.T) {
+	test.TestFileReader_Read(t, dirs)
+}
 
-	t.Run("should read file", func(t *testing.T) {
-		dir := createTempDir(t)
-		osDir := deebee.OsDir(dir)
-		writeFile(t, dir, "existing-file")
-		// when
-		reader, err := osDir.FileReader("existing-file")
-		require.NoError(t, err)
-		defer reader.Close()
-		// then
-		actualData, err := ioutil.ReadAll(reader)
-		require.NoError(t, err)
-		assert.Equal(t, actualData, data)
-	})
+func TestOsDir_DirExists(t *testing.T) {
+	test.TestDir_DirExists(t, dirs)
+}
+
+func TestOsDir_Mkdir(t *testing.T) {
+	test.TestDir_Mkdir(t, dirs)
+}
+
+func TestOsDir_Dir(t *testing.T) {
+	test.TestDir_Dir(t, dirs)
+}
+
+func TestOsDir_ListFiles(t *testing.T) {
+	test.TestDir_ListFiles(t, dirs)
 }
 
 func createTempDir(t *testing.T) string {
 	dir, err := ioutil.TempDir("", "test")
 	require.NoError(t, err)
 	return dir
-}
-
-func writeFile(t *testing.T, dir, file string) {
-	f := filepath.Join(dir, file)
-	err := ioutil.WriteFile(f, data, 0666)
-	require.NoError(t, err)
 }
