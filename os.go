229a76ec84499a664d9eef622f601736628cc657
Jacek Olszak <jacekolszak@gmail.com>
2021-01-24 17:26:39 +0100

    Implement new Dir abstraction in OsDir

--- os.go
+++ os.go
@@ -1,7 +1,9 @@
 package deebee
 
 import (
+	"errors"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -9,12 +11,15 @@ import (
 type OsDir string
 
 func (o OsDir) FileReader(name string) (io.ReadCloser, error) {
+	if name == "" {
+		return nil, errors.New("empty file name")
+	}
 	return os.Open(o.path(name))
 }
 
 func (o OsDir) FileWriter(name string) (FileWriter, error) {
 	flags := os.O_CREATE | os.O_EXCL | os.O_WRONLY
-	return os.OpenFile(o.path(name), flags, 0666)
+	return os.OpenFile(o.path(name), flags, 0664)
 }
 
 func (o OsDir) path(name string) string {
@@ -22,17 +27,35 @@ func (o OsDir) path(name string) string {
 }
 
 func (o OsDir) DirExists(name string) (bool, error) {
-	panic("implement me")
+	f, err := os.Stat(o.path(name))
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return f.IsDir(), nil
 }
 
 func (o OsDir) Mkdir(name string) error {
-	panic("implement me")
+	err := os.Mkdir(o.path(name), 0775)
+	if os.IsExist(err) {
+		return nil
+	}
+	return err
 }
 
 func (o OsDir) Dir(name string) Dir {
-	panic("implement me")
+	return OsDir(o.path(name))
 }
 
 func (o OsDir) ListFiles() ([]string, error) {
-	panic("implement me")
+	var files []string
+	fileInfos, err := ioutil.ReadDir(string(o))
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range fileInfos {
+		if !f.IsDir() {
+			files = append(files, f.Name())
+		}
+	}
+	return files, nil
 }
