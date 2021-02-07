bdf910e4bcff09b46b77b88a13fb767ea68f4eb0
Jacek Olszak <jacekolszak@gmail.com>
2021-02-07 17:02:56 +0100

    Implement basic compacter using go routine.

--- compaction/compaction.go
+++ compaction/compaction.go
@@ -1,23 +1,59 @@
 package compaction
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/jacekolszak/deebee/store"
 )
 
 func Strategy(options ...StrategyOption) store.Option {
 	return func(s *store.Store) error {
+		compacter := &Compacter{}
 		for _, option := range options {
 			if option == nil {
 				return nil
 			}
-			if err := option(&Compacter{}); err != nil {
+			if err := option(compacter); err != nil {
 				return err
 			}
 		}
-		return nil
+		return store.Compacter(compacter.start)(s)
 	}
 }
 
 type StrategyOption func(*Compacter) error
 
 type Compacter struct{}
+
+func (c *Compacter) start(ctx context.Context, state store.State) {
+	go func() {
+		for {
+			select {
+			case <-state.Updates():
+				versions, _ := state.Versions()
+				for _, version := range versions {
+					_ = version.Remove()
+				}
+			}
+		}
+	}()
+}
+
+func MaxVersions(max int) StrategyOption {
+	return func(compacter *Compacter) error {
+		if max < 0 {
+			return fmt.Errorf("negative max in compaction.MaxVersions: %d", max)
+		}
+		return nil
+	}
+}
+
+func MinVersions(min int) StrategyOption {
+	return func(compacter *Compacter) error {
+		if min < 0 {
+			return fmt.Errorf("negative max in compaction.MaxVersions: %d", min)
+		}
+		return nil
+	}
+}
