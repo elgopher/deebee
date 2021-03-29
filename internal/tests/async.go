f60bb43585bdd720310c9deb308cdeb016ac53ba
Jacek Olszak <jacekolszak@gmail.com>
2021-03-29 23:48:54 +0200

    Implement replicator

---
+++ internal/tests/async.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func RunAsync(f func()) Async {
+	done := make(chan struct{})
+	go func() {
+		f()
+		close(done)
+	}()
+	return Async{done: done}
+}
+
+type Async struct {
+	done <-chan struct{}
+}
+
+func (a *Async) WaitOrFailAfter(t *testing.T, timeout time.Duration) {
+	select {
+	case <-a.done:
+	case <-time.After(timeout):
+		assert.FailNow(t, "timeout waiting for async function to finish")
+	}
+}
